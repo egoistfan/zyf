3fb199fd1a5a7f89d2429811d258d837f84cb635
zouyufeng <1418689104@qq.com>
2018-09-05 22:42:19 +0800

    zuoye

---
+++ caaa/src/main/java/cap/action/FirstServlet.java
@@ -0,0 +1,22 @@
+package cap.action;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/first")
+public class FirstServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        out.print("hello servlet");
+        out.close();
+    }
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
